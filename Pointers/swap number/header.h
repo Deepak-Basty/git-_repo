961342fe3f75fcc04567c21ceb33cc1b96785764
Deepak-Basty <bastydeepak@gmail.com>
2019-02-26 14:48:33 +0530

    this is the header file

---
+++ Pointers/swap number/header.h
@@ -0,0 +1,21 @@
+#ifndef _HEADER_H_
+#define _HEADER_H_
+
+#include <stdint.h>
+
+typedef uint8_t temporary_variable;
+typedef uint8_t integer;
+
+integer variable_1,variable_2;
+temporary_variable temp;
+
+void Print_swap(uint8_t *num1,uint8_t *num2);
+void print_swap();
+
+#include "Print_swap_number.c"
+
+#endif
+
+
+
+
